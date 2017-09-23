2f992b766d77e25f918792a6137f134c3089d88d
Kyle <k90armstrong@gmail.com>
2017-09-23 09:18:25 -0600

    removes useless code

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,15 +3,7 @@ window.addEventListener("scroll", function () {
     // check if larger than height of the top bar
     if (scrollY >= 40) {
         document.getElementById("main-header").className = 'main-header rotate fix'  // if it is larger than add the class to flip and move the header
-        // document.getElementById("main-header").style.top = "120px";
-        // document.getElementById("main-header").style.left = "-290px";
-        // document.getElementById("main-header").style.transform = "rotate(-90deg)";
-        // document.getElementById("main-header").style.position = "fixed";
     } else {
         document.getElementById("main-header").className = 'main-header' // if it is not remove the class to flip it
-        // document.getElementById("main-header").style.top = "10px";
-        // document.getElementById("main-header").style.left = "0px";
-        // document.getElementById("main-header").style.transform = "";
     }
-
 })
